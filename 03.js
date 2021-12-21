e6c5ce6a9f191c8ecc93aeb37ce982119088d819
Nataliya <lisa.volma1984@gmail.com>
2021-12-21 23:45:41 +0300

    21.12

---
+++ 03.js
@@ -0,0 +1,8 @@
+//Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(myString) {
+    const lowercaseInput = myString.toLowerCase();
+    const forward = lowercaseInput;
+    const backward = lowercaseInput.split("").reverse().join("");
+    return forward === backward;
+  }
