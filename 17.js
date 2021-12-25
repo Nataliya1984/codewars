5e223ae85ce41c59982ed4a10c1b2f0f62dc16a8
Nataliya <lisa.volma1984@gmail.com>
2021-12-25 22:56:59 +0300

    25.12

---
+++ 17.js
@@ -0,0 +1,11 @@
+//Ensure question
+
+//Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+function ensureQuestion(s) {
+  return s.includes("?") ? s : s + "?";
+}
+
+console.log(ensureQuestion("")); //"?"
+console.log(ensureQuestion("Yes")); //"Yes?"
+console.log(ensureQuestion("No?")); //"No?"
