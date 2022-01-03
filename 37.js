7881a016054e9ac303110815d2cd7d490d4b87e4
Nataliya <lisa.volma1984@gmail.com>
2022-01-03 12:48:13 +0300

    Grasshopper - Check for factor

---
+++ 37.js
@@ -0,0 +1,9 @@
+// Grasshopper - Check for factor
+
+//This function should test if the factor is a factor of base.
+
+//Return true if it is a factor or false if it is not.
+
+function checkForFactor(b, f) {
+  return Number.isInteger(b / f) ? true : false;
+}
