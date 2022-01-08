c5da88caa33655add6f35605f58efe93ce05f842
Nataliya <lisa.volma1984@gmail.com>
2022-01-08 22:39:48 +0300

    CSV representation of array

---
+++ 54.js
@@ -0,0 +1,9 @@
+//CSV representation of array
+
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+function toCsvText(array) {
+  let str = array.join("\n");
+
+  return str;
+}
